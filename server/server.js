b0ced4162f3ff60b06ddd427d0ead722beed4c02
Alex Matranga <24361338+amatranga@users.noreply.github.com>
2024-12-24 16:29:50 -0600

    [fix]: make app work with yarn

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path';
 import { router } from './routes/index.js';
 
 const app = express();
@@ -8,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'public/index.html'));
+});
+
 app.use('/api', router);
 
 app.listen(PORT, () => {
