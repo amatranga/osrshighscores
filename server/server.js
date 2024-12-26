3c05f540445d147e105eacdecc9a986ee911882f
Alex Matranga <24361338+amatranga@users.noreply.github.com>
2024-12-26 17:51:50 -0600

    [fix]: change to require

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { router } from './routes/index.js';
+// import express from 'express';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import path from 'path';
+// import { router } from './routes/index.js';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const router = require('./routes/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
