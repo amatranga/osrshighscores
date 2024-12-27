79bb1e1ac336a1fd52eb709bca8e1eda3aa9cdf7
Alex Matranga <24361338+amatranga@users.noreply.github.com>
2024-12-26 19:08:09 -0600

    chore: heroku

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { router } from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(bodyParser.json());
