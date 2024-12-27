a7e47079a538000a854c2444e788c366aa5b0253
Alex Matranga <24361338+amatranga@users.noreply.github.com>
2024-12-27 16:15:01 -0600

    [fix]: fix path to build file

--- server/server.js
+++ server/server.js
@@ -9,10 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const currentDirectory = path.resolve(__dirname);
+const buildDirectory = path.resolve(currentDirectory, '..', 'build');
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join('..', __dirname, 'build')));
+app.use(express.static(buildDirectory));
 
 app.use('/api', router);
 
